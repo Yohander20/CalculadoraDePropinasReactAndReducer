30a15657f18d1fdf5a0912e384e35032e883a260
Yohander Teran <yteran@intra.polybarq.com>
2024-11-05 11:37:42 -0400

    first commit

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,14 +1,16 @@
 import {useMemo} from 'react'
 import {OrderItem} from "../types/index"
 import { formatCurrency } from '../helpers'
+import { Dispatch } from 'react'
+import { OrderActions } from '../reducers/order-reducer'
 
 type OrderTotalsProps={
     order:OrderItem[],
     tip:number,
-    placeOrder:()=>void
+    dispatch:Dispatch<OrderActions>
 }
 
-export default function OrderTotals({order,tip,placeOrder}:OrderTotalsProps) {
+export default function OrderTotals({order,tip,dispatch}:OrderTotalsProps) {
 
   const subtotalAmount=useMemo(()=>order.reduce((total,item)=>total + (item.quantity * item.price),0),[order])  
   const tipAmount=useMemo(()=>subtotalAmount*tip,[tip,order])
@@ -31,7 +33,7 @@ export default function OrderTotals({order,tip,placeOrder}:OrderTotalsProps) {
     <button
     className='w-full bg-black p-3 uppercase text-white font-bold mt-10 disabled:opacity-10'
     disabled={totalAmount===0}
-    onClick={placeOrder}
+    onClick={()=>dispatch({type:'placer-order'})}
     >
         Guardar Orden
     </button>
--- src/components/TipPorcentajeForm.tsx
+++ src/components/TipPorcentajeForm.tsx
@@ -1,4 +1,5 @@
 import type {Dispatch} from 'react'
+import { OrderActions } from '../reducers/order-reducer'
 
 const tipOptions = [
     {
@@ -19,11 +20,11 @@ const tipOptions = [
   ]
 
   type TipPercentageFormProps={
-    setTip: Dispatch<React.SetStateAction<number>>,
+    dispatch:Dispatch<OrderActions>
     tip:number
   }
 
-export default function TipPorcentajeForm({setTip,tip}:TipPercentageFormProps) {
+export default function TipPorcentajeForm({dispatch,tip}:TipPercentageFormProps) {
   return (
     <div>
       <h3 className="font-black text-2xl">Propina:</h3>
@@ -36,7 +37,7 @@ export default function TipPorcentajeForm({setTip,tip}:TipPercentageFormProps) {
                     type="radio"
                     name="tip"
                     value={tipOption.value}
-                    onChange={e=>setTip(+e.target.value)}
+                    onChange={e=> dispatch({type:'add-tip',payload:{value: +e.target.value}})}
                     checked={tipOption.value === tip}
                 />
             </div>
